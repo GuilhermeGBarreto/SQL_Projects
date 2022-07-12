df4a21c28c4e9358b27a01a28aa683d8d2b569f5
Guilherme G. Barreto <108954603+GuilhermeGBarreto@users.noreply.github.com>
2022-07-12 20:47:45 -0300

    Add files via upload

---
+++ Projeto Locadora/Análises/AnalisesDasEstruturas.sql
@@ -0,0 +1,26 @@
+use video_locadora;
+
+select f.titulo as titulo, count(a.id_aluguel) as Locacoes from filmes as f
+join alugueis as a
+on f.id_filme = a.id_filme
+group by f.titulo
+order by Locacoes DESC;
+
+select genero, round(avg(preco_aluguel), 2) as Preço_Médio
+from filmes
+group by genero
+order by Preço_Médio DESC;
+
+
+describe clientes;
+describe alugueis;
+
+select avg(nota) from alugueis;
+
+select f.titulo as Titulo, round(a.nota, 1) as Nota from filmes as f
+join alugueis as a
+on f.id_filme = a.id_filme
+where a.nota >= (select avg(nota) as Nota from alugueis)
+group by f.titulo
+order by Nota DESC;
+
